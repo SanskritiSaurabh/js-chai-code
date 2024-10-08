f70b09f4ffa907d25a7e0af48d28907ea180615c
Sanskriti Saurabh <sanskritisaurabh907@gmail.com>
2024-10-08 08:32:40 +0000

    Conversion of types

---
+++ 01_basics/typeconversion.js
@@ -0,0 +1,12 @@
+let name="sanskriti"
+let score=10
+let divion=null
+
+console.log(name);
+console.log(typeof name)
+let divInNumber= Number(divion)
+console.log( divInNumber);
+
+let login="q"
+let boollogin=Boolean(login)
+console.log(boollogin);
